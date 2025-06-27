9bd6b158c3dc119fd4995167bb83590f5b35fe2b
Nadid Wasique <76746289+DaTaklaCatto@users.noreply.github.com>
2025-06-27 16:15:10 +0600

    Added Maintainance Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ function initMenu() {
   let menuButton = document.querySelector(".menu-button");
   let menuButtonTexts = menuButton.querySelectorAll("p");
   let menuButtonIcon = menuButton.querySelector(".menu-button-icon");
-
+  let navLogo = document.querySelector(".nav-logo");
   let tl = gsap.timeline();
 
   const openNav = () => {
@@ -79,6 +79,72 @@ function initMenu() {
   });
 }
 
+function initScrollTrigger() {
+  ScrollTrigger.create({
+    trigger: "#hero",
+    start: "top top",
+    end: "90% top",
+    onEnter: () => {
+      gsap.to(".nav-logo", { color: "black", fill: "black" });
+    },
+    onLeaveBack: () => {
+      gsap.to(".nav-logo", { color: "white", fill: "white" });
+    },
+  });
+
+  gsap.to(".logo-path", {
+    scrollTrigger: {
+      trigger: "#hero",
+      start: "top top",
+      end: "90% top",
+      scrub: true,
+      onUpdate: (self) => {
+        if (self.progress >= 0.9) {
+          gsap.to(".logo-path", {
+            fill: "#000000", // Change to whatever color you want
+            duration: 0.3,
+          });
+        } else {
+          gsap.to(".logo-path", {
+            fill: "#FAFAFA",
+            duration: 0.3,
+          });
+        }
+      },
+    },
+  });
+
+  gsap.to([".menu-text", ".close-text", ".menu-stroke"], {
+    scrollTrigger: {
+      trigger: "#hero",
+      start: "top top",
+      end: "90% top",
+      scrub: true,
+      onUpdate: (self) => {
+        if (self.progress >= 0.9) {
+          gsap.to(".menu-text, .close-text", {
+            color: "#000000",
+            duration: 0.3,
+          });
+          gsap.to(".menu-stroke", {
+            stroke: "#000000",
+            duration: 0.3,
+          });
+        } else {
+          gsap.to(".menu-text, .close-text", {
+            color: "#FAFAFA",
+            duration: 0.3,
+          });
+          gsap.to(".menu-stroke", {
+            stroke: "#FAFAFA",
+            duration: 0.3,
+          });
+        }
+      },
+    },
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   setInterval(() => {
     let now = new Date(new Date().getTime() + 6 * 3600000);
@@ -88,4 +154,5 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("clock").innerText = `${h}:${m} ${ampm}`;
   }, 1000);
   initMenu();
+  initScrollTrigger();
 });
